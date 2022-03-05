607b744b80113d4cea8bfa308ddac9c109a44477
shreya-sajeev <100975522+shreya-sajeev@users.noreply.github.com>
2022-03-05 15:16:52 +0530

    Update and rename India's average GDP per capita to Question 5.sql

--- Question 5.sql
+++ Question 5.sql
@@ -1,4 +1,4 @@
-What is India's average GDP per capita income across the number of years provided by the data set?
+--What is India's average GDP per capita income across the number of years provided by the data set?
 SELECT AVG(value) AS AVERAGE_GDP, COUNT(value) AS NUMBER_OF_YEARS 
 FROM `bigquery-public-data.world_bank_intl_education.international_education` 
 WHERE country_name = "India" AND indicator_name = "GDP per capita (current US$)" 
