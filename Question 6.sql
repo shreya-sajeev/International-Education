5148078e45979cbbab073d53b71ed853cdc13cb8
shreya-sajeev <100975522+shreya-sajeev@users.noreply.github.com>
2022-03-05 15:24:01 +0530

    Update and rename Female population growth rate, of age group 15-64 to Question 6.sql

--- Question 6.sql
+++ Question 6.sql
@@ -1,3 +1,3 @@
-For the year 2000, what is the female population growth rate, of age group 15-64, across all countries, in ascending order?
+--For the year 2000, what is the female population growth rate, of age group 15-64, across all countries, in ascending order?
 SELECT * FROM `bigquery-public-data.world_bank_intl_education.international_education` 
 WHERE   year = 2000 AND  indicator_name = "Population, ages 15-64, female" ORDER BY value ASC
