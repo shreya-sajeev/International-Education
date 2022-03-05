880ae5c2b7c9331f1970f7fce85b969e53a5de2d
shreya-sajeev <100975522+shreya-sajeev@users.noreply.github.com>
2022-03-05 15:21:48 +0530

    Update and rename Years of theoretical duration of secondary education across countries to Question 3.sql

--- Question 3.sql
+++ Question 3.sql
@@ -1,3 +1,3 @@
-In the year 2016, how many years of theoretical duration of secondary level of education was accepted across the globe?
+--In the year 2016, how many years of theoretical duration of secondary level of education was accepted across the globe?
 SELECT * FROM `bigquery-public-data.world_bank_intl_education.international_education` WHERE   year = 2016 
 AND  indicator_name = "Theoretical duration of secondary education (years)" ORDER BY value ASC
