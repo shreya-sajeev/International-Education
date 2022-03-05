366976774240ff63a2e44036c0d693ffdc5b1029
shreya-sajeev <100975522+shreya-sajeev@users.noreply.github.com>
2022-03-05 15:20:41 +0530

    Update and rename Key educational indicators in Brazil to Question 1.sql

--- Question 1.sql
+++ Question 1.sql
@@ -1,3 +1,3 @@
-What are all the key educational indicators that have been used for the country of Brazil?
+--What are all the key educational indicators that have been used for the country of Brazil?
 SELECT * FROM `bigquery-public-data.world_bank_intl_education.international_education` 
 WHERE country_name = "Brazil" 
