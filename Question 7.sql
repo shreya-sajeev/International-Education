cca46d6974c1fbeb04ffbea00888732b385ed506
shreya-sajeev <100975522+shreya-sajeev@users.noreply.github.com>
2022-03-05 15:24:43 +0530

    Update and rename Countries with greater than average internet user numbers to Question 7.sql

--- Question 7.sql
+++ Question 7.sql
@@ -1,4 +1,4 @@
-What are the countries that have internet users (per 100) greater than the average number of internet users globally?
+--What are the countries that have internet users (per 100) greater than the average number of internet users globally?
 SELECT * FROM `bigquery-public-data.world_bank_intl_education.international_education` WHERE indicator_name = "Internet users (per 100 people)" 
 AND value>(SELECT AVG(value) FROM `bigquery-public-data.world_bank_intl_education.international_education` 
 WHERE indicator_name = "Internet users (per 100 people)")
